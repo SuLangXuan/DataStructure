124ad105b2e4bba08032864f90a659a2025df82b
SuperXuan <604625554@qq.com>
2019-08-27 22:19:59 +0800

    实现合并两个有序的单链表，合并后链表依然有序。

--- DataStructure/src/main/java/com/lx/datastructure/ds3_linkedlist/Test2.java
+++ DataStructure/src/main/java/com/lx/datastructure/ds3_linkedlist/Test2.java
@@ -1,5 +1,7 @@
 package com.lx.datastructure.ds3_linkedlist;
 
+import java.util.Stack;
+
 /**
  * 合并两个有序的单链表，合并之后的链表依然有序【课后练习.】
  */
@@ -20,29 +22,40 @@ public class Test2 {
         linkedList2.addByOrder(new Node(4,"bb"));
         linkedList2.addByOrder(new Node(7,"cc"));
 
+
+        Node newHead = new Node();
+        Stack<Node> stack = new Stack<Node>();
+
         Node temp1 = linkedList1.head.next;
+
         while (true){
             if (temp1 == null) break;
-            linkedList3.add(temp1);
             System.out.println(temp1);
+//            linkedList3.addByOrder(temp1);
+            stack.add(temp1);
+
+
+
             temp1 = temp1.next;
         }
 
+        System.out.println("=============");
         Node temp2 = linkedList2.head.next;
+
         while (true){
             if (temp2 == null) break;
-            linkedList3.add(temp2);
+            System.out.println(temp2);
+            stack.add(temp2);
             temp2 = temp2.next;
         }
 
-//        linkedList1.list();
-//
-//        System.out.println("========");
-//
-//        linkedList2.list();
-//
+
         System.out.println("=-============");
-//
+
+        while (stack.size()>0){
+//            System.out.println(stack.pop());
+            linkedList3.addByOrder(stack.pop());
+        }
         linkedList3.list();
     }
 
